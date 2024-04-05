f6c436f7e6f79f2830ee3ba2c7ce42c36510b183
SneakyBuzZ <kaushikx304@gmail.com>
2024-04-06 03:47:59 +0530

    Navigation improved for mobile

--- src/lib/constants/navigation.ts
+++ src/lib/constants/navigation.ts
@@ -1,4 +1,4 @@
-export const navItems = [
+export const navItemsBottom = [
   {
     id: 1,
     label: "Home",
@@ -13,12 +13,44 @@ export const navItems = [
   },
   {
     id: 3,
+    label: "Addpost",
+    route: "/addpost",
+    imageIcon: "/plus_icon.png",
+  },
+  {
+    id: 4,
     label: "People",
     route: "/people",
     imageIcon: "/people_icon.png",
   },
   {
+    id: 5,
+    label: "Saved",
+    route: "/saved",
+    imageIcon: "/save_icon.png",
+  },
+];
+export const navItems = [
+  {
+    id: 1,
+    label: "Home",
+    route: "/",
+    imageIcon: "/home_icon.png",
+  },
+  {
+    id: 2,
+    label: "Explore",
+    route: "/explore",
+    imageIcon: "/explore_icon.png",
+  },
+  {
     id: 4,
+    label: "People",
+    route: "/people",
+    imageIcon: "/people_icon.png",
+  },
+  {
+    id: 5,
     label: "Saved",
     route: "/saved",
     imageIcon: "/save_icon.png",
---
+++ src/lib/constants/profileStats.ts
@@ -0,0 +1,14 @@
+export const profileStats = [
+  {
+    number: 220,
+    stat: "followers",
+  },
+  {
+    number: 4,
+    stat: "posts",
+  },
+  {
+    number: 302,
+    stat: "following",
+  },
+];
